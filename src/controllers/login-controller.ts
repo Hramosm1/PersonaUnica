3e7b4c1ed0ead0bcf0e02005c80a1766790a32be
Diego Jared  Donis Soto <programacion4@recagua.com>
2022-07-11 10:43:35 -0600

    se corrigio bug que no regresaba el idCobrador

--- src/controllers/login-controller.ts
+++ src/controllers/login-controller.ts
@@ -3,12 +3,11 @@ import { Token } from "../token";
 import { prisma } from "../database";
 export class LoginController {
   public async login(req: Request, res: Response) {
-
     const _Token = new Token();
     const { username, password } = req.body;
     try {
       const result: any = await prisma.$queryRawUnsafe(
-        `SELECT CAST(idCobrador AS INT), estado, CoNombre as nombre, CoApellido as apellido FROM IC_Cobrador WHERE Usuario = '${username}' AND Password = CAST('${password}' AS binary)`
+        `SELECT CAST(idCobrador AS INT) idCobrador, estado, CoNombre as nombre, CoApellido as apellido FROM IC_Cobrador WHERE Usuario = '${username}' AND Password = CAST('${password}' AS binary)`
       );
       if (result.length > 0) {
         const user = result[0];
